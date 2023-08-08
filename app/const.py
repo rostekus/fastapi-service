609fd872a8b19285b2edd4054a1dec652daae553
rostekus <rostyslavmosorov@gmail.com>
2023-08-09 01:01:14 +0200

    chore: added const for model router

--- app/const.py
+++ app/const.py
@@ -1,4 +1,11 @@
+from enum import Enum
 from typing import Final
 
+# OPEN API
 OPEN_API_TITLE: Final = "FastAPI Service"
 OPEN_API_DESCRIPTION: Final = "Example of FastAPI Service"
+
+# Model
+
+MODEL_TAGS: Final[list[str | Enum] | None] = ["Model"]
+MODEL_URL: Final = "model"
