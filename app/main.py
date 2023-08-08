a324fabd94f635b3126d182dbe035bc73340d2b4
rostekus <rostyslavmosorov@gmail.com>
2023-08-09 01:02:57 +0200

    feat: added model router to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.const import OPEN_API_DESCRIPTION, OPEN_API_TITLE
-from app.version import __version__
+from app.routers.model import router as model_router
+from const import OPEN_API_DESCRIPTION, OPEN_API_TITLE
+from version import __version__
 
 app = FastAPI(
     title=OPEN_API_TITLE,
@@ -9,6 +10,8 @@ app = FastAPI(
     version=__version__,
     swagger_ui_parameters={"defaultModelsExpandDepth": -1},
 )
+
+app.include_router(model_router)
 
 
 @app.get("/")
