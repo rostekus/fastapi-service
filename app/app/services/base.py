14ad6a6748c79ead60e71dc6a49af416bf4c8f7d
rostekus <rostyslavmosorov@gmail.com>
2023-08-09 01:00:43 +0200

    feat: implemented service for relevemce model

---
+++ app/app/services/base.py
@@ -0,0 +1,12 @@
+from app.repository.api import ModelDataBase
+
+
+class DataBaseProvider:
+    """Provides instance of database session."""
+
+    def __init__(self, db: ModelDataBase) -> None:
+        self.db = db
+
+
+class BaseService(DataBaseProvider):
+    """Base class for application services."""
---
+++ app/app/services/model.py
@@ -0,0 +1,7 @@
+from app.services.base import BaseService
+
+
+class ModelService(BaseService):
+    def calculate_relevance(self, training_data: list[float]) -> list[float]:
+        """Calculate relevance"""
+        return []
