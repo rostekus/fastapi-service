6d99e80ec5910162d6393e7e73ecb3f0340aefce
rostekus <rostyslavmosorov@gmail.com>
2023-08-12 18:59:42 +0200

    feat: added more pydentic schemas

--- app/app/schemas/model.py
+++ app/app/schemas/model.py
@@ -1,9 +1,27 @@
+from __future__ import annotations
+
 from pydantic import BaseModel
 
 
 class TrainingDataRequestSchema(BaseModel):
     data: list[float]
+    k: int
+    l: int
 
 
 class RelevanceResponseSchema(BaseModel):
+    data: list[float] = []
+    message: dict[str, str] = {}
+
+
+class PredictDataRequestSchema(BaseModel):
     data: list[float]
+    id: str
+
+
+class PredictDataResponseSchema(BaseModel):
+    data: list[float]
+
+
+class GetAllModelsResponseSchema(BaseModel):
+    data: list[str]
